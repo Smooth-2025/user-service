7314987de94eb03e596aea18ef4c8cb694d846fe
suin <73993670+whl5105@users.noreply.github.com>
2025-08-16 17:47:00 +0900

    feat: 차량연동  Repository 구현 #16

--- src/main/java/com/smooth/smooth_backend_user/repository/UserVehicleRepository.java
+++ src/main/java/com/smooth/smooth_backend_user/repository/UserVehicleRepository.java
@@ -3,5 +3,12 @@ package com.smooth.smooth_backend_user.repository;
 import com.smooth.smooth_backend_user.entity.UserVehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserVehicleRepository extends JpaRepository<UserVehicle, Long> {
+
+    Optional<UserVehicle> findByUserId(Long userId);
+    boolean existsByUserId(Long userId);
+    boolean existsByVehicleId(Long vehicleId);
+
 }
