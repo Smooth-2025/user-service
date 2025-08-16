175322c20319e544d8746547e6f87b5bcc4bf9c3
suin <73993670+whl5105@users.noreply.github.com>
2025-08-16 18:31:08 +0900

    feat: 차량연동 도메인 동작 메서드 구현 #16

--- src/main/java/com/smooth/smooth_backend_user/entity/UserVehicle.java
+++ src/main/java/com/smooth/smooth_backend_user/entity/UserVehicle.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -19,15 +20,24 @@ public class UserVehicle {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false , unique = true)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "vehicle_id", nullable = false )
+    @JoinColumn(name = "vehicle_id", nullable = false)
     private Vehicle vehicle;
 
     @CreationTimestamp
     @Column(name = "linked_at", nullable = false, updatable = false)
     private LocalDateTime linkedAt;
+
+    public static UserVehicle createUserVehicle(User user, Vehicle vehicle) {
+        UserVehicle userVehicle = new UserVehicle();
+        userVehicle.setUser(user);
+        vehicle.addUserVehicle(userVehicle);
+        return userVehicle;
+    }
 }
--- src/main/java/com/smooth/smooth_backend_user/entity/Vehicle.java
+++ src/main/java/com/smooth/smooth_backend_user/entity/Vehicle.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"plate_number", "imei"}))
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "plate_number", "imei" }))
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor
@@ -28,12 +28,34 @@ public class Vehicle {
     @Column(name = "imei", nullable = false, length = 15)
     private String imei;
 
-    @CreatedDate
+    @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = false)
+    @OneToMany(mappedBy = "vehicle", cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE },
+            orphanRemoval = true)
     private List<UserVehicle> userVehicles = new ArrayList<>();
+
+
+    public void addUserVehicle(UserVehicle userVehicle) {
+        if (!userVehicles.contains(userVehicle)) {
+            userVehicles.add(userVehicle);
+            userVehicle.setVehicle(this);
+        }
+    }
+
+    public void removeUserVehicle(UserVehicle userVehicle) {
+        if (userVehicles.remove(userVehicle)) {
+            userVehicle.setVehicle(null);
+        }
+    }
+
+    public static Vehicle createVehicle(String plateNumber, String imei) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.plateNumber = plateNumber;
+        vehicle.imei = imei;
+        vehicle.userVehicles = new ArrayList<>();
+        return vehicle;
+    }
+
 }
-
-
