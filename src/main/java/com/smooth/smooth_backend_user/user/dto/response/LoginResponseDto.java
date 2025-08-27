f79ee312500b08b7fd22cd7b4a3c364c4115f6ad
131c2 <bg3189@naver.com>
2025-08-27 21:13:26 +0900

    rafactor: CommonResponseDto 상속 제거로 응답 구조 단순화

--- src/main/java/com/smooth/smooth_backend_user/user/dto/response/LoginResponseDto.java
+++ src/main/java/com/smooth/smooth_backend_user/user/dto/response/LoginResponseDto.java
@@ -2,32 +2,21 @@ package com.smooth.smooth_backend_user.user.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class LoginResponseDto extends CommonResponseDto {
+public class LoginResponseDto {
     private Long userId;
     private String name;
     private String token;
 
     public static LoginResponseDto success(Long userId, String name, String token) {
         LoginResponseDto response = new LoginResponseDto();
-        response.setSuccess(true);
-        response.setMessage("로그인 성공");
         response.setUserId(userId);
         response.setName(name);
         response.setToken(token);
-        return response;
-    }
-
-    public static LoginResponseDto error(String message) {
-        LoginResponseDto response = new LoginResponseDto();
-        response.setSuccess(false);
-        response.setMessage(message);
         return response;
     }
 }
